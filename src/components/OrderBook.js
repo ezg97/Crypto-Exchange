96752f86906f043966a4b0281b82e1bab53a975c
ezg97 <elijahguerrero97@gmail.com>
2022-06-29 22:35:20 -0500

    updated name

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -44,8 +44,8 @@ const showOrderBook = (props) => {
     <tbody>
       {orderBook.sellOrders.map((order) => renderOrder(order, props))}
       <tr>
-        <th>DAPP</th>
-        <th>DAPP/ETH</th>
+        <th>MTN</th>
+        <th>MTN/ETH</th>
         <th>ETH</th>
       </tr>
       {orderBook.buyOrders.map((order) => renderOrder(order, props))}
--- src/components/Trades.js
+++ src/components/Trades.js
@@ -35,8 +35,8 @@ class Trades extends Component {
               <thead>
                 <tr>
                   <th>Time</th>
-                  <th>DAPP</th>
-                  <th>DAPP/ETH</th>
+                  <th>MTN</th>
+                  <th>MTN/ETH</th>
                 </tr>
               </thead>
               { this.props.filledOrdersLoaded ? showFilledOrders(this.props.filledOrders) : <Spinner type="table" />}
