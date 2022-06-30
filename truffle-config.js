82bddead9ec40bb10b61459545622311e7e21302
ezg97 <elijahguerrero97@gmail.com>
2022-06-29 21:04:07 -0500

    Added UI & deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,11 @@
 require("babel-register");
 require("babel-polyfill");
 require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const privateKeys = process.env.PRIVATE_KEYS || "";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
 
@@ -10,6 +15,23 @@ module.exports = {
      port: 7545,            // Ganache
      network_id: "*",       // Any network (default: none)
     },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          // Private key
+          // privateKeys.split(","), // Array account of private keys
+          mnemonic,
+          // URL to Ethereum node
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      // main net is 1
+      network_id: 42,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
   contracts_directory: './src/contracts',
   contracts_build_directory: './src/abis/',
